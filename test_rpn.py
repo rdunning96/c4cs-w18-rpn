c6108cf04b53efb75a8e6beec647c7cca2523fbe
rdunning96 <rdunning96@gmail.com>
2018-03-07 23:41:57 -0500

    added test for exponentiation

--- test_rpn.py
+++ test_rpn.py
@@ -1,8 +1,10 @@
 import unittest
 import rpn
 
-
 class TestBasics(unittest.TestCase):
+	def test_exponent(self):
+		result = rpn.calculate('2 3 ^')
+		self.assertEqual(8, result)
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
 		self.assertEqual(2, result)
