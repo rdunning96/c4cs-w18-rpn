0dc27f46b1d8a4a9cd4f50c38b0f52ccfea89298
rdunning96 <rdunning96@gmail.com>
2018-03-07 23:49:20 -0500

    changed exponent

--- rpn.py
+++ rpn.py
@@ -1,21 +1,35 @@
 #/usr/bin/env python3
 
+import operator
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.trudiv,
+	'^': operator.pow,
+}
 
 def calculate(arg):
 	stack = list()
 	for token in arg.split():
 		try:
-			value = int(token)
-			stack.append(value)
+			token = int(token)
+			stack.append(token)
 		except ValueError:
+			function = operators[token]
 			arg1 = stack.pop()
 			arg2 = stack.pop()
-			return arg1 + arg2
+			result = function(arg1, arg2)
+			stack.append(result
+		print(stack)
+	return stack.pop() 
 
 
 def main():
 	while True:
-		print(calculate(input('rpn calc> ')))
+		result = calculate(input("rpn calc> "))
+		print("Result: ", result)
 
 
 if __name__ == '__main__':
